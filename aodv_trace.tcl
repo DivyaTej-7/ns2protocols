e9788ee057307c41689fa85a67f8ccba9c7e1eb9
DivyaTej-7 <teja.at.the.rate.2004hi@gmail.com>
2025-05-21 13:34:56 +0530

    Add files via upload

---
+++ aodv_trace.tcl
@@ -0,0 +1,48 @@
+set ns [new Simulator]
+set tracefile [open aodv_trace.tr w]
+$ns trace-all $tracefile
+set namfile [open aodv_simulation.nam w]
+$ns namtrace-all $namfile
+proc finish {} {
+    global ns tracefile namfile
+    
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    
+    exec nam aodv_simulation.nam &
+    exit 0
+}
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+set n7 [$ns node]
+set n8 [$ns node]
+set n9 [$ns node]
+$ns duplex-link $n0 $n1 2Mb 10ms DropTail
+$ns duplex-link $n1 $n2 2Mb 10ms DropTail
+$ns duplex-link $n2 $n3 2Mb 10ms DropTail
+$ns duplex-link $n3 $n4 2Mb 10ms DropTail
+$ns duplex-link $n4 $n5 2Mb 10ms DropTail
+$ns duplex-link $n5 $n6 2Mb 10ms DropTail
+$ns duplex-link $n6 $n7 2Mb 10ms DropTail
+$ns duplex-link $n7 $n8 2Mb 10ms DropTail
+$ns duplex-link $n8 $n9 2Mb 10ms DropTail
+set opt(adhocRouting) AODV
+set udp0 [new Agent/UDP]
+$ns attach-agent $n4 $udp0
+set null0 [new Agent/Null]
+$ns attach-agent $n8 $null0
+$ns connect $udp0 $null0
+set cbr [new Application/Traffic/CBR]
+$cbr set packetSize_ 512
+$cbr set interval_ 0.2
+$cbr attach-agent $udp0
+$ns at 1.0 "$cbr start"
+$ns at 4.0 "$cbr stop"
+$ns at 5.0 "finish"
+$ns run
